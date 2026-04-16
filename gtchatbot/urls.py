e2fb4facab1bc9db071f75f26d52d9a51e0b07f0
ViceBitz <25victorg@students.harker.org>
2026-04-16 00:37:15 -0400

    add login url paths

--- gtchatbot/urls.py
+++ gtchatbot/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from chat.views import interface, dashboard
+from chat.views import interface, dashboard, login_view, register_view, logout_view, history_view
 
 urlpatterns = [
     path('', interface, name='interface'),
+    path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
+    path('logout/', logout_view, name='logout'),
+    path('history/', history_view, name='history'),
     path('dashboard/', dashboard, name='dashboard'),
     path('admin/', admin.site.urls),
     path('api/chat/', include('chat.urls')),
